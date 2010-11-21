b40188de7bf48f502364dc3494a48a36529c11da
Matthew Grooms <mgrooms@shrew.net>
2010-11-21 20:13:52 +0000

    Correct some issues with the OSX DMG package build script.
    git-svn-id: svn://svn.shrew.net/ike/head@743 1bb156eb-19af-db11-b919-0014c23ffdf2

--- package/macosx/build-package.sh
+++ package/macosx/build-package.sh
@@ -9,18 +9,18 @@ mv build "$VOLNAME"
 
 cp ../../LICENSE.TXT "./$VOLNAME"
 
-/usr/bin/hdiutil create -ov -format UDRW -srcfolder "./$VOLNAME" ./vpn-client-install.dmg
+sudo /usr/bin/hdiutil create -ov -format UDRW -srcfolder "./$VOLNAME" ./vpn-client-install.dmg
 
 mkdir ./vol
 
-/usr/bin/hdiutil attach ./vpn-client-install.dmg -readwrite -noautoopen -mountpoint ./vol/
+sudo /usr/bin/hdiutil attach ./vpn-client-install.dmg -readwrite -noautoopen -mountpoint ./vol/
 /bin/cp ./vpn-client-volume.icns ./vol/.VolumeIcon.icns
 /Developer/Tools/SetFile -a -C ./vol
 
-/usr/bin/hdiutil detach ./vol/
+sudo /usr/bin/hdiutil detach ./vol/
 rmdir ./vol
 
-/usr/bin/hdiutil convert ./vpn-client-install.dmg -format UDZO -o ./vpn-client-install-ro.dmg
+sudo /usr/bin/hdiutil convert ./vpn-client-install.dmg -format UDZO -o ./vpn-client-install-ro.dmg
 /bin/mv -f ./vpn-client-install-ro.dmg ./vpn-client-install.dmg
 
 rm -fr "./$VOLNAME"
